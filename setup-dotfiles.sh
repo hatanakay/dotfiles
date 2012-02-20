c50640e4332e1b3e82ded018e56865b9cf93a46a
hatanakay <hatanaka.yoichi@gmail.com>
2012-02-20 09:30:56 +0900

    add setup.sh

---
+++ setup-dotfiles.sh
@@ -0,0 +1,10 @@
+ln -fs $HOME/dotfiles/.zshrc $HOME/.zshrc
+ln -fs $HOME/dotfiles/.zsh $HOME/.zsh
+ln -fs $HOME/dotfiles/.vimrc $HOME/.vimrc
+ln -fs $HOME/dotfiles/.gvimrc $HOME/.gvimrc
+ln -fs $HOME/dotfiles/.vim $HOME/.vim
+ln -fs $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
+ln -fs $HOME/dotfiles/.gemrc $HOME/.gemrc
+ln -fs $HOME/dotfiles/.irbrc $HOME/.irbrc
+
+
