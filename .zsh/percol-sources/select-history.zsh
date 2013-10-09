64f072088134004b475dec998500df21bd53c5b5
hatanakay <hatanaka.yoichi@gmail.com>
2013-10-09 14:22:54 +0900

    percolの時にhistoryの行番号を削除

--- .zsh/percol-sources/select-history.zsh
+++ .zsh/percol-sources/select-history.zsh
@@ -2,7 +2,7 @@
 function percol-select-history() {
   local tac_cmd
   which gtac &> /dev/null && tac_cmd=gtac || tac_cmd=tac
-  BUFFER=$(history | $tac_cmd | sed 's/^: [0-9]*:[0-9]*;//' \
+  BUFFER=$(history | $tac_cmd | cut -d " " -f3-  \
     | percol --match-method migemo --query "$LBUFFER")
   CURSOR=$#BUFFER         # move cursor
   zle -R -c               # refresh
