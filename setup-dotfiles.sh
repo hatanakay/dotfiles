e69afb2e492adac10e6967be1f803b5595855724
hatanakay <hatanaka.yoichi@gmail.com>
2014-03-02 20:10:35 +0900

    add ctags settings

--- setup-dotfiles.sh
+++ setup-dotfiles.sh
@@ -27,6 +27,9 @@ if [ -d $HOME/.tmux ]; then
     unlink $HOME/.tmux
 fi
 
+if [ -d $HOME/.gittmp ]; then
+    unlink $HOME/.gittmp
+fi
 
 #git submodule
 git submodule init
@@ -36,3 +39,4 @@ ln -s $HOME/dotfiles/.vim $HOME/.vim
 ln -s $HOME/dotfiles/.zsh $HOME/.zsh
 ln -s $HOME/dotfiles/.antigen $HOME/.antigen
 ln -s $HOME/dotfiles/.tmux $HOME/.tmux
+ln -s $HOME/dotfiles/.gittmp $HOME/.gittmp
