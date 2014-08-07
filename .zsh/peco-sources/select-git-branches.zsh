e7f04de4557e55c1606958f7bb7ad1bb3e1fbfd1
hatanakay <hatanaka.yoichi@gmail.com>
2014-08-07 10:11:07 +0900

    branch 選択後の正規表現修正

--- .zsh/peco-sources/select-git-branches.zsh
+++ .zsh/peco-sources/select-git-branches.zsh
@@ -1,5 +1,5 @@
 function peco-git-recent-all-branches () {
-    local selected_branch=$(git branch -a | peco | sed -e "s/\* //g" | awk "{print \$1}")
+    local selected_branch=$(git branch -a | peco | sed -e "s/\* //g" | awk "{print \$1}" | sed -e 's/remote\/refs\///g' | sed -e 's/remotes\/origin\///g')
     if [ -n "$selected_branch" ]; then
         BUFFER="git checkout ${selected_branch}"
         zle accept-line
