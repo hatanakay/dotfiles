68cf1bd11ee2fb85fdcb59e24d7a6f0333570f67
hatanakay <hatanaka.yoichi@gmail.com>
2017-08-21 14:27:43 +0900

    fix local settings

--- .zsh/peco-sources/vim.zsh
+++ .zsh/peco-sources/vim.zsh
@@ -1,8 +1,8 @@
 function peco-vim () {
     if [ -e ./.git ]; then
-        git ls-files | peco | xargs sh -c 'vim "$0" < /dev/tty'
+        git ls-files | peco | xargs sh -c 'nvim "$0" < /dev/tty'
     else
-        find . -type f  | peco | xargs sh -c 'vim "$0" < /dev/tty'
+        find . -type f  | peco | xargs sh -c 'nvim "$0" < /dev/tty'
     fi
     zle clear-screen
 }
